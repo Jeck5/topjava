fbcdb3aeb2c918cf7c5b413ec32826ee08db352c
Lions <jeck000005@gmail.com>
2018-03-18 00:41:10 +0300

    meals: index for user_id & datetime

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -36,3 +36,6 @@ CREATE TABLE meals
   datetime    TIMESTAMP DEFAULT now() NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX meals_unique_user_id_datetime_idx
+  ON meals (user_id, datetime);
