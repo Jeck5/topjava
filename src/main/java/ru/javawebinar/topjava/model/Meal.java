b5847d8af7e8cdfa134c04ac4dc2cec982ae2050
Lions <jeck000005@gmail.com>
2018-03-24 21:27:27 +0300

    @UniqueConstraint for user_id, datetime

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -18,7 +18,7 @@ import java.time.LocalTime;
         @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
 })
 @Entity
-@Table(name = "meals")
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
 
     public static final String UPDATE = "Meal.update";
