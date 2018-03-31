c7dceb223e40cfa52e8dc0bd90ec16dd43bbc1c6
Lions <jeck000005@gmail.com>
2018-03-31 22:45:10 +0300

    HW05 tests JDBC JPA DATAJPA were done

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -6,27 +6,24 @@ public class Profiles {
             JPA = "jpa",
             DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
+    public static final String REPOSITORY_IMPLEMENTATION = JDBC;
 
     public static final String
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
     //  Get DB profile depending of DB driver in classpath
-    public static String[] getActiveDbProfiles() {
-        String profiles[] = new String[2];
+    public static String getActiveDbProfile() {
         try {
             Class.forName("org.postgresql.Driver");
-            profiles[0] = POSTGRES_DB;
+            return POSTGRES_DB;
         } catch (ClassNotFoundException ex) {
             try {
                 Class.forName("org.hsqldb.jdbcDriver");
-                profiles[0] = HSQL_DB;
+                return Profiles.HSQL_DB;
             } catch (ClassNotFoundException e) {
                 throw new IllegalStateException("Could not find DB driver");
             }
         }
-        profiles[1] = REPOSITORY_IMPLEMENTATION;
-        return profiles;
     }
 }
