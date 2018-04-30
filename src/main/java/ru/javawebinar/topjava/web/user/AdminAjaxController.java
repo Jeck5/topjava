3904a25d9ad760c61646b774367aa069f394940b
Lions <jeck000005@gmail.com>
2018-04-30 23:13:30 +0300

    HW08 task 5

--- src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
@@ -34,4 +34,10 @@ public class AdminAjaxController extends AbstractUserController {
             super.create(user);
         }
     }
+
+    @PostMapping("/{id}")
+    public void setEnabled(@PathVariable("id") int id, @RequestParam("enabled") Boolean enabled) {
+        service.updateEnabled(id,enabled);
+    }
+
 }
--- src/main/webapp/resources/js/userDatatables.js
+++ src/main/webapp/resources/js/userDatatables.js
@@ -39,4 +39,36 @@ $(function () {
         ]
     });
     makeEditable();
+    makeEditableForUsers();
 });
+
+function makeEditableForUsers() {
+    $(".enabled").change(function () {
+        var enabled = $(this).is(':checked');
+        var id = $(this).parents('tr').first().attr("id");
+        setEnabled(id,enabled)
+    });
+}
+
+function setEnabled(id,enabled) {
+    $.ajax({
+        type: "POST",
+        url: ajaxUrl+id,
+        data: 'enabled=' + enabled,
+        success: function () {
+            updateTable();
+            successNoty("Enabled changed");
+        }
+    });
+}
+// function enableDisable() {
+//     var checkbox = $(".enabled");
+//     var enabled = checkbox.is(':checked');
+//     var id = checkbox.parents('tr').first().attr("id");
+//     //var id2 = this.parents('tr').first().attr("id");
+//     var tmp=0;
+//
+//
+//    // $(this).checked();
+//
+// }
